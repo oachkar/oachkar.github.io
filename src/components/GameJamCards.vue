74543a7e08dcb6cf5add1af0a0dfe08613b8dbd6
fresh4 <fresh1736@gmail.com>
2025-04-24 00:17:07 -0500

    Initial commit

---
+++ src/components/GameJamCards.vue
@@ -0,0 +1,94 @@
+<!-- GameJamCards.vue -->
+<!-- <script setup></script> -->
+
+<template>
+  <div class="game-jam-cards">
+    <!-- Gelatoss Card -->
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://img.itch.zone/aW1nLzE0Nzk4MTYzLnBuZw==/315x250%23c/zRYdQy.png"
+        class="card-img-top"
+        alt="Gelatoss!"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Gelatoss!</h5>
+        <p class="card-text">A couch competitive game of high velocity slime splitting.</p>
+        <a href="https://fresh4.itch.io/gelatoss" class="btn itch-btn">Download on itch.io</a>
+      </div>
+    </div>
+
+    <!-- Pepper Hermit Card -->
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://img.itch.zone/aW1nLzE3MTMwMzE4LnBuZw==/315x250%23c/0EpDil.png"
+        class="card-img-top"
+        alt="Pepper Hermit"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Pepper Hermit</h5>
+        <p class="card-text">
+          Take on the role of a secret cabal's researcher to develop the ultimate capsaicin potion
+          known to man!
+        </p>
+        <a href="https://fresh4.itch.io/pepper-hermit" class="btn itch-btn"> Play on itch.io </a>
+      </div>
+    </div>
+
+    <!-- Mount Bowlympus Card -->
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://img.itch.zone/aW1nLzE5NjE1NTMxLnBuZw==/315x250%23c/fwR9TC.png"
+        class="card-img-top"
+        alt="Mount Bowlympus"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Mount Bowlympus</h5>
+        <p class="card-text">
+          Roll, smash, and soar through this Synthwave inspired open leveled arcade bowler!
+        </p>
+        <a href="https://fresh4.itch.io/hyperbowl" class="btn itch-btn"> Play on itch.io </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-img-top {
+  transition: 0.25s;
+}
+.card-img-top:hover {
+  transition: 0.25s;
+  scale: 110%;
+  border-radius: 0.5rem;
+}
+
+.itch-btn {
+  background-color: #fa5c5c;
+  color: #fff;
+}
+
+.itch-btn:hover {
+  background-color: #e04e4e;
+  color: #fff;
+}
+
+.game-jam-cards {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: nowrap;
+  gap: 2rem;
+}
+
+.game-jam-cards .card .card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+@media (max-width: 800px) {
+  .game-jam-cards {
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,72 @@
+<script setup>
+import GameJamCards from '../components/GameJamCards.vue'
+</script>
+
+<template>
+  <!-- <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/portfolio">Portfolio</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+  </header> -->
+  <main>
+    <section class="projects">
+      <h2>Game Jam Projects</h2>
+      <p>
+        I've been involved in a number of game jams in which I worked very closely with team members
+        to produce a fully featured demo within a short time. My responsibilities and contributions
+        consisted of the programming and and implementation of game mechanics. The art in Pepper
+        Hermit was mostly made by me.
+      </p>
+      <GameJamCards />
+    </section>
+
+    <section class="about">
+      <div>
+        <h2>About Me</h2>
+        <p>
+          Software, web, and game developer with a passion for user experience and good internal
+          design. <br />
+        </p>
+        <h4>Education</h4>
+        <p>
+          Bachelor of Science in Computer Science from the University of Houston. <br />
+          Master of Science in Cybersecurity from the University of Houston. <br />
+        </p>
+        <h4>Skills</h4>
+        <p>
+          Web: HTML, CSS, JavaScript, Vue.js, React.js, Node.js, Python, SQL <br />
+          Game: Godot, Unity, C#, C++ <br />
+        </p>
+      </div>
+    </section>
+
+    <section>
+      <h2>Links</h2>
+      <p>
+        <a href="https://github.com/fresh4">GitHub</a> •
+        <a href="https://fresh4.itch.io/">Itch</a> •
+        <a href="https://www.linkedin.com/in/omar-achkar/">LinkedIn</a>
+      </p>
+    </section>
+
+    <section class="contact">
+      <h2>Contact Me</h2>
+      <p>
+        For professional inquiries, contact me at
+        <a href="mailto:omar.achkar98@gmail.com"> omar.achkar98@gmail.com </a> <br />
+
+        For academic inquiries, contact me at
+        <a href="mailto:oachkar@uh.edu"> oachkar@uh.edu </a> <br />
+      </p>
+    </section>
+  </main>
+</template>
+
+<style>
+section {
+  margin-top: 2rem;
+}
+</style>
