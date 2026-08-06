c25a190307bc5df337e0902db42f80ae731cc75e
fresh4 <fresh1736@gmail.com>
2026-08-05 23:01:34 -0500

    Add template site to portfolio

--- src/components/WebsitesCards.vue
+++ src/components/WebsitesCards.vue
@@ -3,22 +3,23 @@
     <h2>Websites</h2>
     <p>
       I also develop websites, including the one you're on right now. I like to keep things simple,
-      lightweight, and easy to use. Though I am a sucker for the occasional flashy landing page with
-      scroll animations... in moderation. Check some of them out.
+      lightweight, and easy to use. If you're interested in a custom website, check out my
+      <a href="https://template.oachkar.com" target="_blank">example landing page</a> for more
+      information. I like to work with small or local business owners to kickstart their online
+      presence, but I am open to (and have) worked with medium-to-large sized companies as a
+      contractor to help with existing projects or as a consultant.
     </p>
     <div class="card-container">
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">CLS Stream</h5>
+          <h5 class="card-title">Landing Page Template</h5>
           <p class="card-text">
-            A web application developed for the multi-speciality clinic CLS Health in Webster,
-            Texas. Though it is an internal tool, here is one public facing application. It utilizes
-            React and Bootstrap for the front end, and communication with a backend API that hits a
-            database, the Google maps API, and does a bunch of logic before presenting the necessary
-            informaton.
+            Interested in a custom website? This example landing page is the perfect representation
+            of what I can make for you or your business quickly and affordably. <br />
+            It is built with Astro for blazing fast delivery, Tailwind, and DaisyUI.
           </p>
-          <a href="https://stream.cls.health/match" target="_blank" class="btn">
-            Visit stream.cls.health
+          <a href="https://template.oachkar.com" target="_blank" class="btn">
+            Visit template.oachkar.com
           </a>
         </div>
       </div>
@@ -26,11 +27,11 @@
         <div class="card-body">
           <h5 class="card-title">CLS Health</h5>
           <p class="card-text">
-            While stream.cls.health is an internal facing tool, cls.health is the patient facing
-            page. I currently contribute to and help maintain this site, powered by Next.js,
-            Tailwind, Prismic, and Vercel. I'm responsible for developing new features with a focus
-            on UX and interactivity, and creating components that the internal content team can use
-            to efficiently build and modify pages.
+            cls.health is the patient facing page for CLS Health, a multispecialty clinic in
+            Webster, Texas. I currently maintain this site, powered by Next.js, Tailwind, Prismic,
+            and Vercel. I'm responsible for developing new features with a focus on UX and
+            interactivity, and creating components that the internal content team can use to
+            efficiently build and modify pages.
           </p>
           <a href="https://cls.health" target="_blank" class="btn"> Visit cls.health </a>
         </div>
