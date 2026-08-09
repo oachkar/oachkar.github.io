6511d931dcbe3012131442a885a0cf45ba888336
fresh4 <fresh1736@gmail.com>
2026-08-09 00:23:16 -0500

    Refactor website cards and add images to them

--- src/components/WebsitesCards.vue
+++ src/components/WebsitesCards.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import WebsiteCard from './reusables/WebsiteCard.vue'
+
+const TemplatePNG = new URL('../imgs/template.png', import.meta.url).href
+const CLSPNG = new URL('../imgs/cls.png', import.meta.url).href
+const GenshinPNG = new URL('../imgs/genshin.png', import.meta.url).href
+</script>
+
 <template>
   <section id="sites">
     <h2>Websites</h2>
@@ -10,46 +18,41 @@
       contractor to help with existing projects or as a consultant.
     </p>
     <div class="card-container">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Landing Page Template</h5>
-          <p class="card-text">
-            Interested in a custom website? This example landing page is the perfect representation
-            of what I can make for you or your business quickly and affordably. <br />
-            It is built with Astro for blazing fast delivery, Tailwind, and DaisyUI.
-          </p>
-          <a href="https://template.oachkar.com" target="_blank" class="btn">
-            Visit template.oachkar.com
-          </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">CLS Health</h5>
-          <p class="card-text">
-            cls.health is the patient facing page for CLS Health, a multispecialty clinic in
-            Webster, Texas. I currently maintain this site, powered by Next.js, Tailwind, Prismic,
-            and Vercel. I'm responsible for developing new features with a focus on UX and
-            interactivity, and creating components that the internal content team can use to
-            efficiently build and modify pages.
-          </p>
-          <a href="https://cls.health" target="_blank" class="btn"> Visit cls.health </a>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Genshin Birthdays</h5>
-          <p class="card-text">
-            This one's a simple, single purpose project. In the game "Genshin Impact", characters
-            have birthdays that give you rewards if you log in on that day. This site simply lets
-            you know whose birthday it is today, and also has functionality to alert subscribers via
-            webhooks, who are saved in a MongoDB cluster.
-          </p>
-          <a href="https://genshin-birthdays.com" target="_blank" class="btn">
-            Visit genshin-birthdays.com
-          </a>
-        </div>
-      </div>
+      <WebsiteCard
+        title="Landing Page Template"
+        link="https://template.oachkar.com"
+        link_label="template.oachkar.com"
+        :img="TemplatePNG"
+      >
+        Interested in a custom website? This example landing page is the perfect representation of
+        what I can make for you or your business quickly and affordably. <br />
+        It is built with Astro for blazing fast delivery, Tailwind, and DaisyUI.
+      </WebsiteCard>
+
+      <WebsiteCard
+        title="CLS Health"
+        link="https://cls.health"
+        link_label="cls.health"
+        :img="CLSPNG"
+      >
+        cls.health is the patient facing page for CLS Health, a multispecialty clinic in Webster,
+        Texas. I currently maintain this site, powered by Next.js, Tailwind, Prismic, and Vercel.
+        I'm responsible for developing new features with a focus on UX and interactivity, and
+        creating components that the internal content team can use to efficiently build and modify
+        pages.
+      </WebsiteCard>
+
+      <WebsiteCard
+        title="Genshin Birthdays"
+        link="https://genshin-birthdays.com"
+        link_label="genshin-birthdays.com"
+        :img="GenshinPNG"
+      >
+        This one's a simple, single purpose project. In the game "Genshin Impact", characters have
+        birthdays that give you rewards if you log in on that day. This site simply lets you know
+        whose birthday it is today, and also has functionality to alert subscribers via webhooks,
+        who are saved in a MongoDB cluster.
+      </WebsiteCard>
     </div>
   </section>
 </template>
@@ -60,25 +63,6 @@
   justify-content: space-around;
   flex-wrap: nowrap;
   gap: 2rem;
-}
-
-.card {
-  width: 18rem;
-}
-
-.card .card-body {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.btn {
-  background-color: hsla(160, 100%, 37%, 1);
-  color: white;
-}
-.btn:hover {
-  background-color: rgb(0, 178, 119);
-  color: white;
 }
 
 @media (max-width: 800px) {
---
+++ src/components/reusables/WebsiteCard.vue
@@ -0,0 +1,84 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  link_label: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <div class="card">
+    <img :src="img" class="card-img-top" :alt="title" v-if="img" />
+    <div class="card-body">
+      <div style="display: flex; gap: 0.5rem; flex-direction: column">
+        <h5 class="card-title">{{ title }}</h5>
+        <p class="card-text">
+          <slot></slot>
+        </p>
+      </div>
+      <a :href="link" target="_blank" class="btn"> Visit {{ link_label }} </a>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-img-top {
+  transition: 0.25s;
+  height: 13rem;
+}
+.card-img-top:hover {
+  transition: 0.25s;
+  scale: 110%;
+  border-radius: 0.5rem;
+}
+
+.card {
+  width: 30%;
+  border: none;
+  background-color: var(--color-background);
+  color: var(--color-text);
+}
+
+.card .card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 1rem;
+}
+
+.btn {
+  background-color: hsla(160, 100%, 37%, 1);
+  color: white;
+}
+.btn:hover {
+  background-color: rgb(0, 178, 119);
+  color: white;
+}
+
+@media (max-width: 800px) {
+  .card-container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .card {
+    width: 100%;
+  }
+  .card-img-top {
+    height: auto;
+  }
+}
+</style>
